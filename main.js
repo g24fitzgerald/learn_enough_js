9523249192c7091bb832d3167d6e3d8b88966c5f
Gina Fitzgerald <g24fitzgerald@gmail.com>
2019-10-16 09:18:59 +1100

    functionality and interface more robust

--- main.js
+++ main.js
@@ -1,11 +1,26 @@
 let Phrase = require("mhartl-palindrome");
-let string = prompt("Please enter a string for palindrome testing:")
-let phrase = new Phrase(string);
 
-if (phrase.palindrome()) {
-  alert(`"${phrase.content}" is a palindrome!`);
-} else {
-  alert(`"${phrase.content}" is not a palindrome.`)
+function palindromeTester(event) {
+  //prevents sending info to nonexistent server 
+  event.preventDefault();
+  // let string = prompt("Please enter a string for palindrome testing:")
+  let phrase = new Phrase(event.target.phrase.value);
+  let palindromeResult = document.querySelector("#palindromeResult");
+
+  if (phrase.palindrome()) {
+    palindromeResult.innerHTML = `"<strong>${phrase.content}</strong>" is a palindrome!`;
+  } else {
+    palindromeResult.innerHTML = `"${phrase.content}" is not a palindrome.`;
+  }
 }
 
-// alert(new Phrase("Madam, I'm Adam.").palindrome());
+document.addEventListener("DOMContentLoaded", function() {
+  // let button = document.querySelector("#palindromeTester");
+  // button.addEventListener("click", function() {
+  //   palindromeTester();
+  // });
+  let form = document.querySelector("#palindromeTester");
+  form.addEventListener("submit", function() {
+    palindromeTester(event);
+  });
+});
